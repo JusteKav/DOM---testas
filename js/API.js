4032198790edecc219c729cce365789ff8932549
Juste Kavaliauskaite <justekavaliauskaite2@gmail.com>
2021-11-10 08:49:45 +0200

    4: adding get and delete HTTP methods and cheking in console

---
+++ js/API.js
@@ -0,0 +1,28 @@
+const urlBase = "http://localhost:3000";
+
+class API {
+  static getApartments = (resolve, reject) => {
+    fetch(urlBase + "/Apartments")
+      .then((data) => data.json())
+      .then(resolve)
+      .catch(reject);
+  };
+
+  static deleteApartments = (id, resolve, reject) => {
+    fetch(urlBase + "/Apartments/" + id, { method: "DELETE" })
+      .then((data) => data.json())
+      .then(resolve)
+      .catch(reject);
+  };
+}
+
+API.getApartments(
+  (duomenys) => console.log("Gauti duomenys: ", duomenys),
+  (klaida) => console.error("Kode yra klaida: ", klaida)
+);
+
+// API.deleteApartments(
+//   "2",
+//   (duomenys) => console.log("Gauti duomenys: ", duomenys),
+//   (klaida) => console.error("Kode yra klaida: ", klaida)
+// );
